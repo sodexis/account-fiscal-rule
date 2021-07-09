30b1f54501e7f2e8c425ae3e2045f9164341d2dd
Prabakaran, Sodexis <prabakaran@sodexis.com>
2021-07-09 11:00:54 +0530

    [IMP] account_avatax_exemption: black, isort, prettier

--- account_avatax_exemption/models/partner.py
+++ account_avatax_exemption/models/partner.py
@@ -4,7 +4,6 @@ from odoo import fields, models
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-
     use_commercial_entity = fields.Boolean(compute="_compute_use_commercial_entity")
 
     def _compute_use_commercial_entity(self):
